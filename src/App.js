fa52c2a6a0ee6f95a22caa9f38b04a9ab22f4865
Daodac-Haze <alkhodaryhazimcsgo@gmail.com>
2022-09-14 13:42:58 +0300

    changes

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { Link } from "react-router-dom";
 export default function App() {
   return (
     <div>
-      <h1>Bookkeeper</h1>
       <nav
         style={{
           borderBottom: "solid 1px",
